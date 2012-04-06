da492ceff3f2dec5b6428b4443efa147dc09e6c2
lmpizarro <lmpbici@gmail.com>
2012-04-05 23:59:59 -0300

    comienzo con mqtt

--- Host/serial_listen.c
+++ Host/serial_listen.c
@@ -1,3 +1,7 @@
+//
+//  credits http://www.pjrc.com/teensy/usb_serial.html
+//
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -7,6 +11,24 @@
 #include <unistd.h>
 #include <termios.h>
 #include <time.h>
+#include <mosquitto.h>
+
+       void my_message_callback(void *obj, struct mosquitto_message *message)
+       {
+            if(message->payloadlen){
+                 printf("%s %s\n", message->topic, message->payload);
+            }else{
+                 printf("%s (null)\n", message->topic);
+            }
+            fflush(stdout);
+       }
+
+
+
+
+
+
+
 
 #define BAUDRATE B9600
 
@@ -17,9 +39,33 @@ const char *begin_string="10 second speed test begins";
 const char *end_string="done!\r\n";
 #define END_LEN 7
 
+int errorMensaje (unsigned char countChars){
+  printf ("error de mensaje\n"); 
+  return (1);
+}
+
+void procMensaje (unsigned char countChars, char *bufRecepc){
+    struct tm *newtime;
+    time_t ltime;
+ 
+    /* Get the time in seconds */
+    time(&ltime);
+    /* Convert it to the structure tm */
+    newtime = localtime(&ltime);
+  
+  printf("%d/%d/%d-%d:%d:%d-%s\n",
+                                   newtime->tm_year+1900, 
+                                   newtime->tm_mon+1, 
+                                   newtime->tm_mday, 
+                                   newtime->tm_hour,  
+                                   newtime->tm_min, 
+                                   newtime->tm_sec, 
+                                   bufRecepc);
+}
+
 int main(int argc, char **argv)
 {
-	char buf[64];
+	char buf[64], bufRecepc[32];
 	int port,k;
 	int n, sum=0, mciclos = 0;
 	struct termios settings;
@@ -52,26 +98,27 @@ int main(int argc, char **argv)
                 sleep(2);
 		n = read(port, buf, 64);
                 if (n>0){ // Hay datos en el buffer
-                   printf ("<-- %d -->   ",n);
                    for (k=0; k < n; k++){
                        if (buf[k] == ':'){
                           countChars = 0;
-                          printf ("Comienzo\n");
+                          bufRecepc[countChars] = buf[k];
+                          countChars = countChars + 1;
                        } else {
-                               // fin de mensaje 0x0A, anterior 0x0D
-                                if (buf[k]!= 0x0A){
-                                  countChars = countChars + 1;
-                                }else{
-                                     if (countChars != 20)
-                                        printf("Error mensaje \n");
-                                     else
-                                        printf ("fin %d \n", countChars);
-                                   countChars = 0;
-                                }
+                          // fin de mensaje 0x0A, anterior 0x0D
+                          if (buf[k]!= 0x0A){
+                            bufRecepc[countChars] = buf[k];
+                            countChars = countChars + 1;
+                          }else {
+                             countChars = countChars + 1;
+                             bufRecepc[countChars] = buf[k];
+                             if (countChars != 21) {
+                               errorMensaje(countChars);
+                             } else {
+                               procMensaje (countChars, bufRecepc);
+                             }
+                          }
                        }
-                                   
-                       printf ("%c",buf[k]);
-                   }
+                   }  //  cierra el for
                 }else { // no hay datos en el buffer
                   mciclos = mciclos + 1;
                   if (mciclos > 50)
